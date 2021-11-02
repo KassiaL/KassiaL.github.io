38a3e9a8b9a396bf544d4758e983afd6c81454f7
KassiaL <muffin.lover.games@gmail.com>
2021-11-02 14:26:04 +0300

    vk5

--- @vkontakte/vk-bridge/dist/types/src/types/deprecated.d.ts
+++ @vkontakte/vk-bridge/dist/types/src/types/deprecated.d.ts
@@ -1,74 +1,56 @@
-import {
-  AnyRequestMethodName,
-  AnyReceiveMethodName,
-  AnyRequestMethodNameWithProps,
-  AnyRequestMethodNameWithoutProps,
-  AnyMethodName,
-  AnyIOMethodName,
-  AnyRequestOnlyMethodName,
-  AnyReceiveOnlyMethodName,
-} from './bridge';
+import { AnyRequestMethodName, AnyReceiveMethodName, AnyRequestMethodNameWithProps, AnyRequestMethodNameWithoutProps, AnyMethodName, AnyIOMethodName, AnyRequestOnlyMethodName, AnyReceiveOnlyMethodName } from './bridge';
 import { CommunityWidgetType } from './data';
-
 /**
  * Name of a method that can be sent.
  * @alias AnyRequestMethodName
  * @deprecated
  */
-export type RequestMethodName = AnyRequestMethodName;
-
+export declare type RequestMethodName = AnyRequestMethodName;
 /**
  * Name of a method that can be received.
  * @alias AnyReceiveMethodName
  * @deprecated
  */
-export type ReceiveMethodName = AnyReceiveMethodName;
-
+export declare type ReceiveMethodName = AnyReceiveMethodName;
 /**
  * Name of a method that can be only sent.
  * @alias AnyRequestOnlyMethodName
  * @deprecated
  */
-export type RequestOnlyMethodName = AnyRequestOnlyMethodName;
-
+export declare type RequestOnlyMethodName = AnyRequestOnlyMethodName;
 /**
  * Name of a method that can be only received.
  * @alias AnyReceiveOnlyMethodName
  * @deprecated
  */
-export type ReceiveOnlyMethodName = AnyReceiveOnlyMethodName;
-
+export declare type ReceiveOnlyMethodName = AnyReceiveOnlyMethodName;
 /**
  * Name of a method which contains properties
  * @alias AnyRequestMethodNameWithProps
  * @deprecated
  */
-export type RequestMethodNameWithProps = AnyRequestMethodNameWithProps;
-
+export declare type RequestMethodNameWithProps = AnyRequestMethodNameWithProps;
 /**
  * Name of a method which doesn't contain properties
  * @alias AnyRequestMethodNameWithoutProps
  * @deprecated
  */
-export type RequestMethodNameWithoutProps = AnyRequestMethodNameWithoutProps;
-
+export declare type RequestMethodNameWithoutProps = AnyRequestMethodNameWithoutProps;
 /**
  * Type of any method name.
  * @alias AnyMethodName
  * @deprecated
  */
-export type MethodName = AnyMethodName;
-
+export declare type MethodName = AnyMethodName;
 /**
  * The name of the method that can be both sent and received.
  * @alias AnyIOMethodName
  * @deprecated
  */
-export type IOMethodName = AnyIOMethodName;
-
+export declare type IOMethodName = AnyIOMethodName;
 /**
  * Community widget type
  * @alias CommunityWidgetType
  * @deprecated
  */
-export type WidgetType = CommunityWidgetType;
+export declare type WidgetType = CommunityWidgetType;
